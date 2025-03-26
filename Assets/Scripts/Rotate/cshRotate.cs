1f927c304975961d7a2477a8e8bfd05a10209c69
LEESEJIN <stacrft@gmail.com>
2025-03-26 20:22:28 +0900

    전자 공전 홀로그램
    전자 공전 홀로그램 및 로테이트 스크립트 하나로 합침

--- Assets/Scripts/Rotate/cshRotate.cs
+++ Assets/Scripts/Rotate/cshRotate.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate1 : MonoBehaviour
+public class cshRotate : MonoBehaviour
 {
+    public Vector3 direction;
     public Transform Element;
     public float speed;
 
     private void Update()
     {
-        transform.RotateAround(Element.position, Vector3.up, speed * Time.deltaTime);
+        transform.RotateAround(Element.position,direction , speed * Time.deltaTime);
     }
-
 
 }
