355bed968186cf3466f776fa2f10b86959f68038
junhk09 <black09248@gmail.com>
2025-05-12 17:31:20 +0900

    저울 완성

---
+++ Assets/Scripts/Scale/BtnScale.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnScale : MonoBehaviour
+{
+    public GameObject scale1;
+    public GameObject scale2;
+    public ScaleMove ScaleMove;
+    public ScaleMove2 ScaleMove2;
+
+   public void cmpScale()
+    {
+         if (ScaleMove.isScale == true && ScaleMove2.isScale2 == true)
+        {
+            scale1.transform.position = new Vector3(scale1.transform.position.x, 0.7f, scale1.transform.position.z);
+            scale2.transform.position = new Vector3(scale2.transform.position.x, 0.7f, scale2.transform.position.z);
+
+        }
+        else if (ScaleMove.isScale == true)
+        {
+            scale1.transform.position = new Vector3(scale1.transform.position.x, 0.7f, scale1.transform.position.z);
+          
+        }
+        else if (ScaleMove2.isScale2 == true) {
+
+            scale2.transform.position = new Vector3(scale2.transform.position.x, 0.7f, scale2.transform.position.z); 
+
+          }
+       
+        else
+        {
+           ScaleMove.isScale = false;
+            ScaleMove2.isScale2 = false;
+            scale1.transform.position = new Vector3(scale1.transform.position.x, 0.8f, scale1.transform.position.z);
+            scale2.transform.position = new Vector3(scale2.transform.position.x, 0.8f, scale2.transform.position.z);
+        }
+    }
+
+
+
+}
