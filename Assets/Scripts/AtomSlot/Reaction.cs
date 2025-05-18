c20d91f4504e374f06a0b657337d7c3ddaf574e2
LEESEJIN <stacrft@gmail.com>
2025-05-19 00:20:45 +0900

    분자 콜리더 변경 및 코드 수정
    분자 콜리더 변경 및 코드 수정

--- Assets/Scripts/AtomSlot/Reaction.cs
+++ Assets/Scripts/AtomSlot/Reaction.cs
@@ -1,19 +1,3 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-
-//public class Reaction
-//{
-//    public Dictionary<string, int> atomInput;   // 왼쪽 검사대
-//    public Dictionary<string, int> atomOutput;  // 오른쪽 검사대
-
-//    public Reaction(Dictionary<string, int> input, Dictionary<string, int> output)
-//    {
-//        atomInput = input;
-//        atomOutput = output;
-//    }
-//}
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,17 +12,5 @@ public class Reaction
     {
         atomInput = input;
         atomOutput = output;
-        Debug.Log("새로운 Reaction 생성됨: " + DictionaryToString(input) + " -> " + DictionaryToString(output));
-    }
-
-    private string DictionaryToString(Dictionary<string, int> dict)
-    {
-        string s = "{";
-        foreach (KeyValuePair<string, int> kvp in dict)
-        {
-            s += kvp.Key + "=" + kvp.Value + ", ";
-        }
-        s = s.TrimEnd(',', ' ') + "}";
-        return s;
     }
 }
