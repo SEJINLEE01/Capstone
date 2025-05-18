e0a62fb7aa8840485bc2b9442373b82d7c2cff5b
junhk09 <black09248@gmail.com>
2025-05-18 16:20:25 +0900

    저울 완성

--- Assets/Scripts/AtomSlot/CheckButton.cs
+++ Assets/Scripts/AtomSlot/CheckButton.cs
@@ -7,42 +7,44 @@ public class CheckButton : MonoBehaviour
 {
     public AtomSlot leftSlot;
     public AtomSlot rightSlot;
-
-   //Reaction.cs라는 스크립트 있음
+    public GameObject[] LeftUI;
+    public GameObject[] RightUI;
+    public GameObject[] CorrectUI;
+    //Reaction.cs라는 스크립트 있음
     public List<Reaction> reactions = new List<Reaction>()
     {
         // 2H2 + O2 → 2H2O
-        new Reaction(new Dictionary<string, int> { { "H", 4 }, { "O", 2 } }, new Dictionary<string, int> { { "H", 4 }, { "O", 2 } }),
+        new Reaction(new Dictionary<string, int> { { "H2", 2 }, { "O2", 1 } }, new Dictionary<string, int> { { "H2O", 2 } }),
 
         // C + O2 → CO2
-        new Reaction(new Dictionary<string, int> { { "C", 1 }, { "O", 2 } }, new Dictionary<string, int> { { "C", 1 }, { "O", 2 } }),
+        new Reaction(new Dictionary<string, int> { { "C", 1 }, { "O2", 1 } }, new Dictionary<string, int> { { "CO2", 1 } }),
 
         // 2Na + Cl2 → 2NaCl
-        new Reaction(new Dictionary<string, int> { { "Na", 2 }, { "Cl", 2 } }, new Dictionary<string, int> { { "Na", 2 }, { "Cl", 2 } }),
+        new Reaction(new Dictionary<string, int> { { "Na", 2 }, { "Cl2", 1 } }, new Dictionary<string, int> { { "NaCl", 2 } }),
 
         // 2Mg + O2 → 2MgO
-        new Reaction(new Dictionary<string, int> { { "Mg", 2 }, { "O", 2 } }, new Dictionary<string, int> { { "Mg", 2 }, { "O", 2 } }),
+        new Reaction(new Dictionary<string, int> { { "Mg", 2 }, { "O2", 1 } }, new Dictionary<string, int> { { "MgO", 2 } }),
 
         // Si + O2 → SiO2
-        new Reaction(new Dictionary<string, int> { { "Si", 1 }, { "O", 2 } }, new Dictionary<string, int> { { "Si", 1 }, { "O", 2 } }),
+        new Reaction(new Dictionary<string, int> { { "Si", 1 }, { "O2", 1 } }, new Dictionary<string, int> {  { "SiO2", 1 } }),
 
         // CH4 + 2O2 → CO2 + 2H2O
-        new Reaction(new Dictionary<string, int> { { "C", 1 }, { "H", 4 }, { "O", 4 } }, new Dictionary<string, int> { { "C", 1 }, { "H", 4 }, { "O", 4 } }),
+        new Reaction(new Dictionary<string, int> { { "CH4", 1 },{"O2",2 } }, new Dictionary<string, int> { { "CO2", 1 },  { "H2O", 2 } }),
 
         // 2Al + 3Cl2 → 2AlCl3
-        new Reaction(new Dictionary<string, int> { { "Al", 2 }, { "Cl", 6 } }, new Dictionary<string, int> { { "Al", 2 }, { "Cl", 6 } }),
+        new Reaction(new Dictionary<string, int> { { "Al", 2 }, { "Cl2", 3 } }, new Dictionary<string, int> { { "AlCl3", 2 }}),
 
         // H2 + Cl2 → 2HCl
-        new Reaction(new Dictionary<string, int> { { "H", 2 }, { "Cl", 2 } }, new Dictionary<string, int> { { "H", 2 }, { "Cl", 2 } }),
+        new Reaction(new Dictionary<string, int> { { "H2", 1 }, { "Cl2", 1 } }, new Dictionary<string, int> { { "HCl", 2 } }),
 
         // 2Li + Cl2 → 2LiCl
-        new Reaction(new Dictionary<string, int> { { "Li", 2 }, { "Cl", 2 } }, new Dictionary<string, int> { { "Li", 2 }, { "Cl", 2 } }),
+        new Reaction(new Dictionary<string, int> { { "Li", 2 }, { "Cl2", 1} }, new Dictionary<string, int> { { "LiCl", 2 } }),
 
         // 2Na + F2 → 2NaF
-        new Reaction(new Dictionary<string, int> { { "Na", 2 }, { "F", 2 } }, new Dictionary<string, int> { { "Na", 2 }, { "F", 2 } }),
+        new Reaction(new Dictionary<string, int> { { "Na", 2 }, { "F2", 1 } }, new Dictionary<string, int> { { "NaF", 2 } }),
 
         // Ca + F2 → CaF2
-        new Reaction(new Dictionary<string, int> { { "Ca", 1 }, { "F", 2 } }, new Dictionary<string, int> { { "Ca", 1 }, { "F", 2 } }),
+        new Reaction(new Dictionary<string, int> { { "Ca", 1 }, { "F2", 1} }, new Dictionary<string, int> { { "CaF2", 1 }}),
     };
 
 
@@ -56,16 +58,77 @@ public class CheckButton : MonoBehaviour
         {
             if (Match(leftAtoms, reaction.atomInput) && Match(rightAtoms, reaction.atomOutput))
             {
-                Debug.Log("정답");
                 leftSlot.SetCorrectColor();
                 rightSlot.SetCorrectColor();
+                foreach (GameObject UI in LeftUI)
+                {
+                    UI.SetActive(true);
+                }
+                foreach (GameObject UI in RightUI)
+                {
+                    UI.SetActive(true);
+                }
+                foreach (GameObject UI in CorrectUI)
+                {
+                    UI.SetActive(true);
+                }
                 return;
             }
+            else if (Match(leftAtoms, reaction.atomInput))
+            {
+                foreach (GameObject UI in LeftUI)
+                {
+                    UI.SetActive(true);
+                }
+                foreach (GameObject UI in RightUI)
+                {
+                    UI.SetActive(false);
+                }
+                foreach (GameObject UI in CorrectUI)
+                {
+                    UI.SetActive(false);
+                }
+                leftSlot.SetCorrectColor();
+                rightSlot.SetWrongColor();
+
+                return;
+            }
+            else if (Match(rightAtoms, reaction.atomOutput))
+            {
+                leftSlot.SetWrongColor();
+                rightSlot.SetCorrectColor();
+                foreach (GameObject UI in LeftUI)
+                {
+                    UI.SetActive(false);
+                }
+                foreach (GameObject UI in RightUI)
+                {
+                    UI.SetActive(true);
+                }
+                foreach (GameObject UI in CorrectUI)
+                {
+                    UI.SetActive(false);
+                }
+                return;
+            }
+
         }
 
-        Debug.Log("오답");
+
         leftSlot.SetWrongColor();
         rightSlot.SetWrongColor();
+        foreach (GameObject UI in LeftUI)
+        {
+            UI.SetActive(false);
+        }
+        foreach (GameObject UI in RightUI)
+        {
+            UI.SetActive(false);
+        }
+        foreach (GameObject UI in CorrectUI)
+        {
+            UI.SetActive(false);
+        }
     }
 
 
--- Assets/Scripts/UImove.cs
+++ Assets/Scripts/UImove.cs
@@ -6,11 +6,25 @@ public class UImove : MonoBehaviour
 {
     public GameObject MainUI;
     public GameObject NextUI;
-
+    public GameObject[] ScaleBtn1;
+    public GameObject[] ScaleBtn2;
+    public GameObject[] ScaleBtn3;
     public void goNext()
     {
         MainUI.SetActive(false);
         NextUI.SetActive(true);
+        foreach (GameObject obj in ScaleBtn1)
+        {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in ScaleBtn2)
+        {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in ScaleBtn3)
+        {
+            obj.SetActive(false);
+        }
     }
 
     
