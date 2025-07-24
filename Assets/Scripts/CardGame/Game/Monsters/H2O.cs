26cbed5d9221e0e67ccbdb3a4d051f55756849ac
junhk09 <black09248@gmail.com>
2025-07-24 21:02:09 +0900

    몬스터 초안
    H2O 프리팹만 적용

--- Assets/Scripts/CardGame/Game/Monsters/H2O.cs
+++ Assets/Scripts/CardGame/Game/Monsters/H2O.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
+using TMPro;
 using UnityEngine;
 
 public class H2O : GameMonster
 {
+    public TextMeshProUGUI cohesionTxt;
     void Start()
     {
         MaxHp = 18;
@@ -13,5 +15,10 @@ public class H2O : GameMonster
         attack_turn = 2; // 공격까지 몇턴 기다리는가 
         turn = 0;
     }
+    private void Update()
+    {
+        cohesionTxt.text = "공격력: " + cohesion;
+    }
+
     // + 애만가지는 고유한 무언가 
 }
--- Assets/Scripts/CardGame/Game/Monsters/MonsterSelect.cs
+++ Assets/Scripts/CardGame/Game/Monsters/MonsterSelect.cs
@@ -5,6 +5,7 @@ using Oculus.Interaction;
 public class MonsterSelect : MonoBehaviour
 {
     public InteractableUnityEventWrapper wrapper; //몬스터마다 추가해줘야함
+    public GameObject selectedImg;
     void Start()
     {
         if (wrapper == null)
@@ -15,6 +16,7 @@ public class MonsterSelect : MonoBehaviour
 
     void OnSelected()
     {
+        selectedImg.SetActive(true);
         GameManager.Instance.SetSelectedObject(this.gameObject);
     }
 }
