dbfed8e4dfe4b370424f1568c4189b96fe6a7db7
LEESEJIN <stacrft@gmail.com>
2025-08-04 01:08:58 +0900

    마무리 수정
    마무리 수정

---
+++ Assets/Scripts/CardGame/Game/MonsterCanvas.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCanvas : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject childCanvasObject;
+
+
+    void Start()
+    {
+        Player = GameObject.Find("CenterEyeAnchor");
+        childCanvasObject = transform.Find("Canvas").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        childCanvasObject.transform.position = (Player.transform.forward * 0.1f);
+        childCanvasObject.transform.rotation = Player.transform.rotation;
+    }
+}
